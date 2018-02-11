3683ddff49d7859f8b07fc31b1169b3c1d226250
droiddrivers <36353247+droiddrivers@users.noreply.github.com>
2018-02-11 11:35:41 +0700

    Add files via upload

---
+++ DroidUSBDrivers.js
@@ -0,0 +1,29 @@
+var numposts = 40000;
+var standardstyling = true;
+
+function startpost(json){
+  for (var i = 0; i < numposts; i++){
+    var entry = json.feed.entry[i];
+    var posttitle = entry.title.$t;
+    var posturl;
+    
+    if (i == json.feed.entry.length) break;
+    
+    for (var k = 0; k < entry.link.length; k++){
+      if (entry.link[k].rel == 'alternate'){
+        posturl = entry.link[k].href;break;
+      }
+    }
+        
+    posttitle = posttitle.link(posturl);
+    if (standardstyling) document.write('<li>');
+    
+    document.write(posttitle);
+  }
+  
+  if (standardstyling) document.write('</li>');
+}
+
+function finished(){
+  document.write('<br /><br /><a href="https://www.droiddrivers.com/" style="font-size: 10 px; text-decoration:none; color: #616469;" target="_blank" title="Android USB Drivers">Android USBD Drivers</a>');
+}
